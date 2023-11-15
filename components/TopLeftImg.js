bcb6aa1866886e4685e3765ff8ae26326bd46e73
zkn <knezevic.zare@gmail.com>
2023-11-15 07:55:42 +0100

    added TopLeftImg functionality and styles

--- components/TopLeftImg.js
+++ components/TopLeftImg.js
@@ -1,5 +1,12 @@
+// Next Image
+import Image from "next/image";
+
 const TopLeftImg = () => {
-  return <div>Top Left Image</div>;
+  return (
+      <div className='absolute left-0 top-0 w-[200px] xl:w-[400px] mix-blend-color-dodge z-10 opacity-50'>
+        <Image src='/top-left-img.png' alt="Top Left Image" width={400} height={400} />
+      </div>
+  );
 };
 
 export default TopLeftImg;
